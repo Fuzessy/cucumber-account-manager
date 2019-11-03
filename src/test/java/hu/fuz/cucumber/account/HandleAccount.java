09f95e674a844bc75af4ea47bfc618f6e6e1c687
Fuzessy <fuzesi.zsolti@gmail.com>
2019-11-03 16:50:13 +0100

    + másik táblázat

--- src/test/java/hu/fuz/cucumber/account/HandleAccount.java
+++ src/test/java/hu/fuz/cucumber/account/HandleAccount.java
@@ -45,14 +45,13 @@ public class HandleAccount {
 
     @Then("A felhasználók számlaegyenlege a következő lesz")
     public void a_felhasználók_számlaegyenlege_a_következő_lesz(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new cucumber.api.PendingException();
+        List<Map<String,String>> table = dataTable.asMaps();
+        for(Map<String,String> row : table){
+            Arrays.asList(
+                    row.get("Felhasználó"),
+                    row.get("Egyenleg")).forEach(a -> System.out.print(a + " | "));
+            System.out.println("");
+        }
     }
 
 
